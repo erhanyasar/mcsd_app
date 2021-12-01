568f80993a48a1bc9d9d8e008b6e6e39524532a6
Erhan Yaşar <mail@erhanyasar.com.tr>
2021-12-01 15:41:26 +0300

    deneme commit

--- src/components/Erhan.js
+++ src/components/Erhan.js
@@ -2,13 +2,23 @@ import "../css/Erhan.css";
 
 function Erhan() {
   let merhaba = "Merhaba ";
-  let isim = "";
+  let isim = "Erhan ";
+
+  const satırIslemleri = () => {
+    return (
+      <>
+        <strong>erhan</strong>
+      </>
+    );
+  };
 
   return (
     <>
-      <h1 style={{ backgroundColor: "lightblue" }}>Bu bir başlık</h1>
+      <h1 className="erhan" style={{ color: "lightblue" }}>
+        {merhaba + isim}
+      </h1>
       <input placeholder="Adınızı giriniz..."></input>
-      <p>{merhaba + isim}</p>
+      <p>{satırIslemleri()}</p>
     </>
   );
 }
